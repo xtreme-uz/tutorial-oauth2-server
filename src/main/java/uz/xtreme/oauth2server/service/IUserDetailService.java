1344f0b04787a773b3ad4fe479489f953ba6cced
xtreme-uz <rustambekov.mail.box@gmail.com>
2019-12-26 17:05:39 +0500

    Init

--- src/main/java/uz/xtreme/oauth2server/service/IUserDetailService.java
+++ src/main/java/uz/xtreme/oauth2server/service/IUserDetailService.java
@@ -1,11 +1,9 @@
 package uz.xtreme.oauth2server.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import uz.xtreme.oauth2server.model.AuthUserDetail;
 import uz.xtreme.oauth2server.model.User;
